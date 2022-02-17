1956ba70d65d343c65c7bc5c13860789405a1d24
Sojin <voise.ksing@gmail.com>
2022-02-17 09:25:07 +0900

    22.02.17 study

---
+++ pybo/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
+from .models import Question
+
+# Create your views here.
+def index(request):
+
+
+    question_list = Question.objects.order_by('create_date')
+    context = {'question_list':question_list}
+    return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+
+
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'pybo/question_detail.html', context)

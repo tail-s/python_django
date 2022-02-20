e8a9bb2f912d8106fe5e6958185d2e017f9d6132
Sojin <voise.ksing@gmail.com>
2022-02-20 20:59:32 +0900

    22.02.20 study

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'pybo.apps.PyboConfig'
+    'common.apps.CommonConfig'
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+# 로그인/로그아웃 성공후 이동하는 URL
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
--- mydjango/urls.py
+++ mydjango/urls.py
@@ -20,4 +20,6 @@ from pybo import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('pybo/', include('pybo.urls')),
+    path('common/', include('common.urls')),
+    path('', views.index, name='index'),  # '/' 에 해당되는 path
 ]
